404ebe1dd99ed6564dbf4f83cafe935630dfb1c8
Abbigail Merrill <abbigail.merrill@gmail.com>
2022-10-19 20:22:12 -0400

    updated to help student with responsiveness / flex

--- cardDetailsForm/js/app.js
+++ cardDetailsForm/js/app.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
-    $(function() {
-        var submit = $('#submit');
-        
-        submit.click(function(){
-            var name = $('#name').value;
-            // var 
-        });
-    });
+    jQuery('#name');
+
+    
 });

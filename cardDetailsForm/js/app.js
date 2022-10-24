61abbe30c92f0f999f079ec11079222d7d02b2e4
Abbigail Merrill <abbigail.merrill@gmail.com>
2022-10-23 21:39:45 -0400

    Resizing main sections now works

--- cardDetailsForm/js/app.js
+++ cardDetailsForm/js/app.js
@@ -1,11 +1,11 @@
 window.addEventListener('resize', () => {
-    if($(window).width() > 525){
+    if($(window).width() > 745){
         $(".one").removeClass('top');
         $(".two").removeClass('bottom');
         $(".one").addClass('left');
         $(".two").addClass('right');
         console.log('The window is bigger than 525')
-} else if ($(window).width() < 525){
+} else if ($(window).width() < 745){
         $(".one").removeClass('left');
         $(".two").removeClass('right');
         $(".one").addClass('top');

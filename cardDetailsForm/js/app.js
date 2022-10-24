11088fabc15ef35bcdef9bca266e504363b1a235
Abbigail Merrill <abbigail.merrill@gmail.com>
2022-10-23 21:36:20 -0400

    updating more styles

--- cardDetailsForm/js/app.js
+++ cardDetailsForm/js/app.js
@@ -1,4 +1,18 @@
+window.addEventListener('resize', () => {
+    if($(window).width() > 525){
+        $(".one").removeClass('top');
+        $(".two").removeClass('bottom');
+        $(".one").addClass('left');
+        $(".two").addClass('right');
+        console.log('The window is bigger than 525')
+} else if ($(window).width() < 525){
+        $(".one").removeClass('left');
+        $(".two").removeClass('right');
+        $(".one").addClass('top');
+        $(".two").addClass('bottom');
+} 
+});
+
 $(document).ready(function(){
-    
     
 });

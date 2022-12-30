d44f430e928cbf90d5f564b0d47171dd9d165616
Abbigail Merrill <abbigail.merrill@gmail.com>
2022-12-29 21:39:07 -0500

    design updates

--- cardDetailsForm/js/app.js
+++ cardDetailsForm/js/app.js
@@ -1,32 +1,62 @@
-// Establishing that content will not load until document is ready
-$(document).ready(
-    function(){
-    // Grabs the input that we will be grabbing value from
-    $('input').keyup(function(){
-        // sets value equal to the active inputs value
+// Fills the name section
+function nameFill(){
+    $('#name').keyup(function(){
         var value = $(this).val();
-        // Sets the inputs innerHTML to whatever is in the value input
-        $('.name').text(value);
-        // runs the function again every time the input keys up
 
-        // next function here
-        // get value of the input field
-            var newval = '';
+        $('.name .fontAdjust').text(value);
+    }).keyup();
+}
 
-            // write regex to remove any space
-            value = value.replace(/\s/g, '');
+// Fills the number section
+function numberFill(){
+$('#cardNum').keyup(function(){
+    
+    var value = $(this).val();
+        var newval = '';
 
-            // iterate through each numver
-            for(var i = 0; i < value.length; i++) {
+        // write regex to remove any space
+        value = value.replace(/\s/g, '');
 
-                // add space if modulus of 4 is 0
-                if(i%4 == 0 && i > 0) newval = newval.concat(' ');
+        // iterate through each numver
+        for(var i = 0; i < value.length; i++) {
 
-                // concatenate the new value
-                newval = newval.concat(value[i]);
-            }
+            // add space if number of charcters = 4
+            if(i%4 == 0 && i > 0) newval = newval.concat(' ');
 
-                // update the final value in the html input
-                console.log(newval);
-            }).keyup();
-    });
+            // concatenate the new value
+            newval = newval.concat(value[i]);
+        }
+            $('.cardNumber').text(newval);
+
+            $('#cardNum').on('keypress', function (e) {
+                var maxlength = $(this).prop('maxlength');
+                if (maxlength !== -1) {  
+                    // Prevent execute statement for non-set maxlength prop inputs
+                    var length = $(this).val().trim().length;
+
+                    if (length + 1 > maxlength) 
+                    e.preventDefault();
+                }
+            });
+        }).keyup();
+};
+
+function monthFill(){
+    $('#month').keyup(function(){
+        var value = $(this).val();
+        $('.monthText').text(value);
+    }).keyup();
+
+    // if(value >= 12){
+    //     alert('Month value cannot exceed 12');
+    //     $('#month').value = '';
+    // }
+}
+
+$(document).ready(
+
+nameFill(),
+numberFill(),
+monthFill()
+
+)
